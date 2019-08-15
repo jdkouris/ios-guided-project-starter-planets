92680cc734b0bd4d7d87f67943f9017d99184711
John Kouris <jdkouris@gmail.com>
2019-08-14 19:48:01 -0700

    Reload the collectionView data in viewWillAppear so pluto appears/disappears based on user selection

--- Planets/View Controllers/PlanetsCollectionViewController.swift
+++ Planets/View Controllers/PlanetsCollectionViewController.swift
@@ -15,6 +15,12 @@ class PlanetsCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.reloadData()
+    }
 
     /*
     // MARK: - Navigation
